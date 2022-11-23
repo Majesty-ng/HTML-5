37965291fcc64aec08bc5ac31ff8497b54ae2fbb
Majesty-ng <113725699+Majesty-ng@users.noreply.github.com>
2022-11-23 10:11:12 +0100

    some new work

---
+++ js/intro/datatypes/main.js
@@ -0,0 +1,57 @@
+alert("21 savage")
+let currency  = "USD"
+console.log(currency)
+
+//rules while creating a variable
+//variables should not start with a digit
+// - there should be no white space between your variable name
+//paschal, camel,kebab case for css, snake case
+for(var i =1; i<21;i++){
+    console.log(i)
+}
+let price_product_name = "that nigga";
+console.log (price_product_name)
+var header = "header"
+document.writeln(header)
+for(var c = 0; c<header.length;c++){
+    document.writeln("*")
+}
+let abool = 3.2456
+document.write(abool)
+const names = ["john", "mary", "tyndale", 36, "rest,", true];
+
+const human = {
+    head: 1,
+    complexion: ["fair","dark"],
+    mortal: true
+};
+//FUNCTION
+//ESS
+function myfunc(){
+    console.log("my_function")
+}
+//execute|invoke|call
+myfunc();
+//ES6
+//arrow function
+const my_function = ()=>{
+    console.log("hello nyiga")
+}
+var majesty = ()=>{
+    console.log("hello")
+}
+let name = "majesty"
+console.log(typeof majesty)
+console.log(typeof abool)
+console.log(typeof names)
+console.log("STRING =>",typeof name)
+console.log("OBJECTS =>",typeof human)
+let wah = 0xa
+console.log("NUMBERS =>",typeof wah)
+const isnull = null
+const istrue = true
+console.log("OBJECT =>",typeof isnull)
+console.log(typeof istrue)
+console.log("BOOLEAN => "+ typeof istrue)
+let undef = undefined
+console.log("UDEFINED => "+typeof undef)
---
+++ js/intro/variables/main.js
@@ -0,0 +1,19 @@
+alert("connected")
+
+var name = "jeffery dahmer"
+name = "wow"
+console.log(name)
+var course ;
+course = "javascript"
+console.log(course)
+let  wock = "shell";
+document.writeln(wock)
+
+{
+    var phone = "iphone"
+    console.log(phone)
+}
+const country   = "nigeria"
+countries = `hello`
+console.log(country)
+console.log(countries)
